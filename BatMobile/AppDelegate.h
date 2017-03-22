5dcf24d342923caeb14ae09f85d5ac002440eeea
jianglinshan <jianglinshan@qiyi.com>
2017-03-22 12:24:04 +0800

    Initial Commit

---
+++ BatMobile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BatMobile
+//
+//  Created by jianglinshan@qiyi.com on 2017/3/22.
+//  Copyright © 2017年 jlstmac@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BatMobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BatMobile
+//
+//  Created by jianglinshan@qiyi.com on 2017/3/22.
+//  Copyright © 2017年 jlstmac@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
